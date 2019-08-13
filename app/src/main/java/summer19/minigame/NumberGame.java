2f0bcf9d70bbbfd8d7fd82abe3629557f0f181f7
sherleywang <sherleywang@ymail.com>
2019-08-13 18:24:56 -0500

    Deleted unnecessary information in main activity and created a number game java class to handle game logic

--- app/src/main/java/summer19/minigame/NumberGame.java
+++ app/src/main/java/summer19/minigame/NumberGame.java
@@ -1,7 +1,48 @@
 package summer19.minigame;
 
-public class NumberGame {
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-    
+import java.util.Random;
+
+public class NumberGame extends AppCompatActivity {
+
+    // instance variables
+    private int secret;
+    private int guess;
+    private boolean[] display;
+
+    // constructor with length as number of digits in the secret number
+    public NumberGame () {
+        Random rand = new Random();
+        secret = rand.nextInt(9000) + 1000;
+        display = new boolean[4];
+    }
+
+    public void round() {
+        TextView input = findViewById(R.id.number_input);
+        guess = Integer.parseInt(input.getText().toString());
+        verifyGuess();
+    }
+
+    private void verifyGuess() {
+        String secretString = "" + secret;
+        String guessString = "" + guess;
+        for (int i = 0; i < 4; i++) {
+            display[i] = secretString.charAt(i) == guessString.charAt(i);
+        }
+    }
+
+    public boolean checkWin() {
+        for (boolean bool : display) {
+            if (!bool)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean[] getDisplay() {
+        return display;
+    }
 
 }
