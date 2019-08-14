01251b6cc76fee15238c1c004aa5303008cbcdfd
sherleywang <sherleywang@ymail.com>
2019-08-14 11:09:44 -0500

    Created a win screen

---
+++ app/src/main/java/summer19/minigame/WinScreen.java
@@ -0,0 +1,14 @@
+package summer19.minigame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WinScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_win_screen);
+
+    }
+}
